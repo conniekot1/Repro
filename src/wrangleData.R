14e72f8d74d3e5774c7d6b151fabd31b1c82c8c5
ckot <connie.kot@duke.edu>
2018-12-03 14:22:32 -0500

    change working directory at end

--- src/wrangleData.R
+++ src/wrangleData.R
@@ -1,3 +1,4 @@
+setwd("~/Documents/data/Class/Repro/src")
 # Minimal Wrangling of the data
 gmpts <- bind_rows(gm182, gm182UP)
 colnames(gmpts) <- c('trackNum', 'time', 'longitude', 'latitude', 'status')
